e93b597ca4f9317c7be2b519cc18e8eddb813752
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-01-27 11:45:15 +0530

    Update filteredController.js

--- backend/controllers/filteredController.js
+++ backend/controllers/filteredController.js
@@ -1,49 +1,5 @@
 
 
-
-//  import Product from "../models/adminProducts.js";
-// export const getFilteredProducts = async (req, res) => {
-//   try {
-//     let { category, minPrice, maxPrice, search } = req.query;
-
-//     let query = {};
-
-//     //  CATEGORY (STRING OR ARRAY BOTH)
-//     if (category) {
-//       if (typeof category === "string") {
-//         category = category.split(",");
-//       }
-
-//       query.category = {
-//         $in: category.map(
-//           (c) => new RegExp(`^${c}$`, "i")
-//         ),
-//       };
-//     }
-
-//     //  PRICE
-//     if (minPrice || maxPrice) {
-//       query.price = {};
-//       if (minPrice) query.price.$gte = Number(minPrice);
-//       if (maxPrice) query.price.$lte = Number(maxPrice);
-//     }
-
-//     //  SEARCH
-//     if (search) {
-//       query.name = { $regex: search.trim(), $options: "i" };
-//     }
-
-//     console.log("FINAL QUERY:", query);
-
-//     const products = await Product.find(query);
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
-
 import Product from "../models/adminProducts.js";
 
 export const getFilteredProducts = async (req, res) => {
