bc8c8851bce99e04711533518d89776aa697291a
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-01-27 09:48:51 +0530

    Update filteredController.js

--- backend/controllers/filteredController.js
+++ backend/controllers/filteredController.js
@@ -2,49 +2,6 @@
 
 
  import Product from "../models/adminProducts.js";
-
-// export const getFilteredProducts = async (req, res) => {
-//   try {
-//     let { category, minPrice, maxPrice, search } = req.query;
-
-//     console.log("RAW query:", req.query);
-
-//     let query = {};
-
-//     //  CATEGORY FIX
-// if (category) {
-//   if (typeof category === "string") {
-//     category = category.split(",");
-//   }
-//   query.category = { $in: category };
-// }
-
-
-
-//     //  PRICE
-//     if (minPrice || maxPrice) {
-//       query.price = {};
-//       if (minPrice) query.price.$gte = Number(minPrice);
-//       if (maxPrice) query.price.$lte = Number(maxPrice);
-//     }
-
-//     //  SEARCH
-//     if (search) {
-//       query.name = { $regex: search.trim(), $options: "i" };
-//     }
-
-//     console.log("FINAL QUERY:", query);
-
-//     const products = await Product.find(query);
-//     res.status(200).json(products);
-
-//   } catch (error) {
-//     console.error("Filter error:", error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
 export const getFilteredProducts = async (req, res) => {
   try {
     let { category, minPrice, maxPrice, search } = req.query;
@@ -56,7 +13,12 @@ export const getFilteredProducts = async (req, res) => {
       if (typeof category === "string") {
         category = category.split(",");
       }
-      query.category = { $in: category };
+
+      query.category = {
+        $in: category.map(
+          (c) => new RegExp(`^${c}$`, "i")
+        ),
+      };
     }
 
     //  PRICE
